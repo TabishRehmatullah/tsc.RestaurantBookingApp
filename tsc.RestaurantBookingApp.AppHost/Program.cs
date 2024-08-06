3d3850c8e5d06af96340812c592c6d6e669e8f4c
Tabishrehmatullah <tabishmemon02@gmail.com>
2024-08-06 15:26:35 +0500

    No functionApp

--- tsc.RestaurantBookingApp.AppHost/Program.cs
+++ tsc.RestaurantBookingApp.AppHost/Program.cs
@@ -1,7 +1,4 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.tsc_RestaurantBookingApp_API>("tsc-restaurantbookingapp-api");
-
-builder.AddProject<Projects.FunctionApp>("functionapp");
-
 builder.Build().Run();
